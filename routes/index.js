408898c1a035c770a73c1bd480925c1754799ef2
dylan reaves <dylanmilesreaves@gmail.com>
2026-08-05 14:50:33 -0400

    added empty files for the routes and removed taskRouter

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 // routes/index.js — one place to collect all routers.
 // Lets app.js grab them from here: const { taskRouter } = require('./routes')
 
-const taskRouter = require('./task.routes');
 const authRouter = require('./auth.routes');
+const scheduleItemRouter = require("./scheduleItem-routes")
+const categoryRouter = require("./category-routes")
 
-// Add a new resource? Import its router above and add one line here.
 module.exports = {
-  taskRouter,
   authRouter,
+  scheduleItemRouter,
+  categoryRouter,
 };
